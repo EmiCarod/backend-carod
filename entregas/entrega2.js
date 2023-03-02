887988ab2ca8ca83885462d2b76a0e115abc6b3a
Emiliano Carod <emicarod@gmail.com>
2023-03-02 01:23:55 -0300

    entrega 2

--- entregas/entrega2.js
+++ entregas/entrega2.js
@@ -1,11 +1,22 @@
+import fs from 'fs'
+
 class productManager{
 
-    constructor (){
-        this.products = [];
+    constructor (path){
+        this.path = path;
+
     }
 
-    getProduct () {
-        return this.products;
+    async getProduct () {
+        //parado desde backend-carod/entregas
+        const json = await fs.readFile('./productos.txt',"utf-8");
+        this.produ = JSON.parse(json);
+        return this.getProduct;
+    }
+
+    async guardarProductos (){
+        const json = JSON.stringify(this.produ, null, 2);
+        await fs.writeFile('./productos.txt', json)
     }
 
     addProduct(producto){
@@ -32,8 +43,9 @@ class productManager{
 }
 
 class Producto {
-        constructor(title, description, price, thumbnail, code, stock)
+        constructor(path, title, description, price, thumbnail, code, stock)
         {
+            this.path = path;
             this.title = title;
             this.description = description;
             this.price = price;
@@ -50,9 +62,6 @@ const pruebaProducto = new productManager();
 console.log ('agregare un producto prueba con title = producto prueba, descripcion = este es un producto prueba, price = 200, thumbnail = sin imagen, code = abc123, stock = 25')
 
 pruebaProducto.addProduct(new Producto('producto prueba','este es un producto prueba', '200','sin imagen', 'abc123', '25'));
-
-console.log ('agrego un segundo producto para comprobar que se auto genera un id nuevo para el producto')
-pruebaProducto.addProduct(new Producto('campera','campera de invierno', '200','sin imagen', 'abc234', '10'));
 
 console.log(pruebaProducto.getProduct());
 
